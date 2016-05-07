c7f3f3c6e84b16788f569e85c946b792d4e97311
kquet <quet.kael@gmail.com>
2016-05-06 19:29:32 -0700

    initial commit

---
+++ IceRingersiOS/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  IceRingersiOS
+//
+//  Created by Kael Quet on 2016-05-06.
+//  Copyright © 2016 Kael Quet. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
